dcef8466e7ef9e59aeb9962e004cef1f776cd9ea
顾鹏凌 <pengling.gu@1mifudao.com>
2018-12-14 17:15:04 +0800

    Initial Commit

---
+++ CurriculumVitae/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurriculumVitae
+//
+//  Created by 顾鹏凌 on 2018/12/14.
+//  Copyright © 2018 顾鹏凌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
